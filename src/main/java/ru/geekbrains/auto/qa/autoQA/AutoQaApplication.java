cc31b309fe4c8bda23c3c6b6b50a17ad4c32c5ae
olgasuperstars <olgasuperstar@mail.ru>
2022-09-23 16:33:04 +0600

    Initial commit

---
+++ src/main/java/ru/geekbrains/auto/qa/autoQA/AutoQaApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.auto.qa.autoQA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoQaApplication.class, args);
+	}
+
+}
