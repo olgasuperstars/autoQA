32c085f7faf31621462b2cb1eaca32c92977a753
olgasuperstars <olgasuperstar@mail.ru>
2022-10-14 16:08:56 +0600

    DZ 5 with mistakes

---
+++ src/test/java/ru/geekbrains/auto/qa/autoqa/lesson7/AutoQaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.auto.qa.autoqa.lesson7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
